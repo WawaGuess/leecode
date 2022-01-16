6389cd3c0f2450f0bbd200c58f2b7309233b8977
xueyandong <xyd2020xyd@gmail.com>
2022-01-16 11:41:42 +0800

    【代码改动类型:修改||leecode-001】leecode-026 【描述】删除有序数组中的重复项-双指针

---
+++ src/main/java/simple/Lc026.java
@@ -0,0 +1,54 @@
+package simple;
+
+/**
+ * @author WawaGuess
+ * @project leecode
+ * @Date 1/16/22
+ * 【到下一行】shift + enter
+ * 【定位到某一行】command + L
+ * 【定位到行首/行尾】command + 方向键
+ * 【在下一行插入一个空行】command + enter
+ * 【构造方法/toString/override提示】control + enter
+ */
+
+
+/**
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Lc026 {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 1, 1};
+
+        int i = removeDuplicates(nums);
+        System.out.println(i);
+        System.out.println(nums);
+
+    }
+
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return nums.length;
+        }
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i + 1;
+    }
+}
