cdfa8f973af37881af00ced91f2e8bdcf334ec35
xueyandong <xyd2020xyd@gmail.com>
2022-04-05 20:53:32 +0800

    【代码改动类型:修改||leecode-001】leecode-106 【描述】相交链表

---
+++ src/main/java/simple/Lc106.java
@@ -0,0 +1,44 @@
+package simple;
+
+/**
+ * @author WawaGuess
+ * @project leecode
+ * @Date 4/5/22
+ * 【到下一行】shift + enter
+ * 【定位到某一行】command + L
+ * 【定位到行首/行尾】command + 方向键
+ * 【在下一行插入一个空行】command + enter
+ * 【构造方法/toString/override提示】control + enter
+ */
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ */
+public class Lc106 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+}
